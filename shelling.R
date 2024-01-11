b0e862c54e4bf8f429c7f45e9d9925bcb364189a
Christian Testa <ctesta01@users.noreply.github.com>
2024-01-11 12:45:54 -0500

    Add comments

--- shelling.R
+++ shelling.R
@@ -1,23 +1,32 @@
 library(ggplot2)
 
+# parameters controlling behavior
 grid_size <- 50
 empty_ratio <- 0.33
 threshold <- 0.70
 n_frames <- 80
 
+# initialize random entries into the grid — with one of value 0, 1, or 2 randomly 
 initialize_grid <- function() {
-  grid <- matrix(sample(c(0, 1, 2), grid_size * grid_size, replace = TRUE, prob = c(empty_ratio, (1 - empty_ratio) / 2, (1 - empty_ratio) / 2)), nrow = grid_size)
+  grid <- matrix(
+    sample(c(0, 1, 2), # sample from 0, 1, 2 with replacement 
+           grid_size * grid_size, # dimensions of the grid
+           replace = TRUE, 
+           prob = c(empty_ratio, (1 - empty_ratio) / 2, (1 - empty_ratio) / 2)), 
+    nrow = grid_size)
   return(grid)
 }
 
+# for a given x,y coordinate, count the number of similar neighbors
 count_similar <- function(grid, x, y) {
   race <- grid[x, y]
   similar <- 0
   total <- 0
-  for (dx in -1:1) {
-    for (dy in -1:1) {
+  for (dx in -1:1) { # look one left and one right
+    for (dy in -1:1) { # look one up and one down
       nx <- x + dx
       ny <- y + dy
+      # if considering a valid coordinate
       if (nx >= 1 && nx <= grid_size && ny >= 1 && ny <= grid_size && !(dx == 0 && dy == 0) && grid[nx, ny] != 0) {
         total <- total + 1
         similar <- similar + (grid[nx, ny] == race)
@@ -25,9 +34,11 @@ count_similar <- function(grid, x, y) {
     }
   }
   if (total == 0) return(TRUE)
+  # return whether the proportion of similar neighbors is above the threshold
   return(similar / total >= threshold)
 }
 
+# for each element in the grid, check if it has enough neighbors
 update_grid <- function(grid) {
   unsatisfied <- c()
   for (x in 1:grid_size) {
@@ -38,6 +49,7 @@ update_grid <- function(grid) {
     }
   }
 
+  # go through the "unsatisfied" elements and move them to a random empty location
   empties <- which(grid == 0, arr.ind=TRUE)
   if (length(unsatisfied) > 0) {
     for (i in 1:nrow(unsatisfied)) {
@@ -52,6 +64,7 @@ update_grid <- function(grid) {
   return(grid)
 }
 
+# produce and save a ggplot showing the grid 
 plot_grid <- function(grid, frame) {
   df <- data.frame(expand.grid(x = 1:grid_size, y = 1:grid_size), race = as.factor(c(grid)))
   p <- ggplot(df, aes(x = x, y = y, fill = race)) +
@@ -69,6 +82,11 @@ plot_grid <- function(grid, frame) {
   ggsave(sprintf("frame%03d.png", frame), p, width=500, height=300, unit='px', scale=3)
 }
 
+# main logic: 
+# initialize the grid 
+# for n_frames, update the grid 
+# at each update, plot the grid and save to a file 
+# finally, animate them together using a system() call to ImageMagick 
 run_simulation <- function() {
   grid <- initialize_grid()
   for (frame in 1:n_frames) {
